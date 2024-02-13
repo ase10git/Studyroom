3af8a6f77eb106745f4081a3435114f656d85a58
Seungwan Yu <dbtmddhks@gmail.com>
2024-02-14 03:37:09 +0900

    2024.02.14.03:36

--- Studyroom/src/main/java/advice/SessionCheckAspect.java
+++ Studyroom/src/main/java/advice/SessionCheckAspect.java
@@ -2,10 +2,12 @@ package advice;
 
 import javax.servlet.http.HttpSession;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 
 import dto.UserDTO;
 
@@ -16,12 +18,19 @@ public class SessionCheckAspect {
 	@Autowired
     private HttpSession session;
     
-    @Before("execution(* com.study.study.UserController.*(..))")
+    @Before("execution(* com.study.study.UserController.user_*(..))")
     public void checkSession() {
-        UserDTO current_session = (UserDTO)session.getAttribute("email");
-        if (current_session == null) {
+        UserDTO currentSession = (UserDTO) session.getAttribute("email");
+        if (currentSession == null) {
             throw new IllegalStateException("세션이 만료되었습니다.");
+        }   
+    }
+    
+    @Before("execution(* com.study.study.UserController.user_*(..)) && args(.., model)")
+    public void addDtoToModel(Model model) {
+        UserDTO dto = (UserDTO) model.asMap().get("dto");
+        if (dto != null) {
+            model.addAttribute("dto", dto);
         }
     }
-	
 }
--- Studyroom/target/classes/context/Context_3_dao.java
+++ Studyroom/target/classes/context/Context_3_dao.java
@@ -3,11 +3,13 @@ package context;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import advice.SessionCheckAspect;
 import dao.UserDAO;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class Context_3_dao {
 	
 	// UserDAO bean »ý¼º
