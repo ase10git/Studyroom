a865b1b71f180855349271faa7d19b1e9d786865
ase10git <148038808+ase10git@users.noreply.github.com>
2024-02-17 03:34:02 +0900

    Revert "20240217 03.28 디렉터리 제거"
    This reverts commit fdc6ee2edbec8aeb2d6d34a905d4897517d9feb9.

---
+++ Studyroom-calender/src/main/java/dto/CalendarDTO.java
@@ -0,0 +1,14 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class CalendarDTO {
+	private int id;
+	private int user_id;
+	private int del_flag;
+	
+	private String type;
+	private String datetime;
+	private String comment;
+***REMOVED***
---
+++ Studyroom-calender/src/main/java/dto/CourseDTO.java
@@ -0,0 +1,17 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class CourseDTO {
+	private int id;
+	private int course_id;
+	private int del_flag;
+	private int ref;
+	private int step;
+	
+	private String title;
+	private String content;
+	private String register_date;
+	private String file_name;
+***REMOVED***
