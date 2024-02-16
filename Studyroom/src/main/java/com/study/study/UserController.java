f25f7903554fb67f89d0db6d5e45882feb2a5c35
Seungwan Yu <dbtmddhks@gmail.com>
2024-02-16 23:58:38 +0900

    24.02.16.23:58

--- Studyroom/src/main/java/com/study/study/UserController.java
+++ Studyroom/src/main/java/com/study/study/UserController.java
@@ -1,5 +1,7 @@
 package com.study.study;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import dao.UserDAO;
 import dto.UserDTO;
 import lombok.RequiredArgsConstructor;
-import service.SessionService;
 import util.Common;
 
 @Controller
@@ -17,33 +18,32 @@ public class UserController {
 	
 	final UserDAO user_dao;
 	
-    @Autowired
-    private SessionService sessionService;
+//    @Autowired
+//    private SessionService sessionService;
+	
+	@Autowired
+	HttpSession session;
 	
 	// 사용자 정보 화면 보기
 	@RequestMapping("user_view")
 	public String user_view(Model model) {	
-		
-        if (model != null) {
-            UserDTO dto = sessionService.getUserFromSession();
-            System.out.println("model is not null");
-            if (dto != null) {
-                model.addAttribute("dto", dto); // Add attribute with a name	
-                System.out.println("dto is not null");
-            ***REMOVED*** else {
-            	System.out.println("dto NULL");
-            ***REMOVED***
-        ***REMOVED*** else {
-        	System.out.println("model NULL");
-        ***REMOVED***
-        
+//		UserDTO dto = sessionService.getUserFromSession();
+		UserDTO dto = (UserDTO)session.getAttribute("email");
+		model.addAttribute("dto", dto);	
 		return Common.USER_PATH+"user_view.jsp";
 ***REMOVED***
 	
 	// 사용자 정보 수정하기 전 비밀번호 확인 페이지 이동
 	@RequestMapping("user_pw_auth_form")
 	public String user_pw_auth_form(Model model) {
+		UserDTO dto = (UserDTO)session.getAttribute("email");
+		model.addAttribute("dto", dto);	
 		return Common.USER_PATH+"pw_auth_form.jsp";
+***REMOVED***
+	
+	@RequestMapping("authenticate")
+	public String authenticate() {
+		return "";
 ***REMOVED***
 	
 	// 사용자 정보 수정 페이지 이동	// 미완성
