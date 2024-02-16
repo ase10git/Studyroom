89a852aae3cca4c778c96013a56a529f6a3ba5c9
핑크보물지도 <mong5886@naver.com>
2024-02-16 21:43:38 +0900

    0216/21:43 /calendar

--- Studyroom/src/main/java/dao/CalendarDAO.java
+++ Studyroom/src/main/java/dao/CalendarDAO.java
@@ -14,28 +14,40 @@ public class CalendarDAO {
 
 	final SqlSession sqlSession;
 	
-	 public List<CalendarDTO> calendar_list(int userId) {
-	        return sqlSession.selectList("c.calendar_list", userId);
-	 ***REMOVED***
-	 
-	 public List<CalendarDTO> getCalYearAndMonth(int userId,int year, int month) {
-	        Map<String, Object> params = new HashMap<>();
-	        params.put("userId", userId);
-	        params.put("year", year);
-	        params.put("month", month);
-	        return sqlSession.selectList("c.getCalYearAndMonth", params);
-	 ***REMOVED***
-	 
-	 public CalendarDTO getCalendarEvent(int userId, int id) {
-		    Map<String, Integer> params = new HashMap<>();
-		    params.put("userId", userId);
-		    params.put("id", id);
-		    return sqlSession.selectOne("c.getcalendarEvent", params);
-***REMOVED***
-	 
-	public void deleteEvent(int id) {
-		sqlSession.update("c.calendarEventUpdate",id);
+	public List<CalendarDTO> calendar_list(int user_id) {
+		return sqlSession.selectList("CalendarMapper.getEventsByUserId", user_id);
 ***REMOVED***
 
+	public List<CalendarDTO> getEventsByMonth(int user_id, int year, int month) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("user_id", user_id);
+		params.put("year", year);
+		params.put("month", month);
+		return sqlSession.selectList("CalendarMapper.getEventsByMonth", params);
+***REMOVED***
+	
+	public void deleteEvent(int id) {
+	    sqlSession.update("CalendarMapper.deleteEvent", id);
+***REMOVED***
+
+	public boolean insertEvent(String type, String comment, String datetime, int userId) {
+	    Map<String, Object> paramMap = new HashMap<>();
+	    paramMap.put("userId", userId);
+	    paramMap.put("type", type);
+	    paramMap.put("comment", comment);
+	    paramMap.put("datetime", datetime);
+	    paramMap.put("delFlag", 0); // default
+	    return sqlSession.insert("CalendarMapper.insertEvent", paramMap) == 1;
+***REMOVED***
+	
+	public int updateEvent(int id, String type, String comment) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        params.put("type", type);
+        params.put("comment", comment);
+
+        return sqlSession.update("CalendarMapper.updateEvent", params);
+    ***REMOVED***
+	
 	
 ***REMOVED***
